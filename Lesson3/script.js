65f0181a4609f010458b1e9085f94d722422701d
Vasil Merkul <vasil.merkul@bisnode.com>
2020-04-03 00:59:52 +0200

    lint

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -3,9 +3,9 @@ let money, time;
 
 function start() {
     money = +prompt("Enter your mounth budget, please!");
-    time = prompt("Please, enter date in format YYYY-MM-DD");   
-    
-    while(isNaN(money) || money == "" || money == null){
+    time = prompt("Please, enter date in format YYYY-MM-DD");
+
+    while (isNaN(money) || money == "" || money == null) {
         money = +prompt("Enter your mounth budget, please!");
     }
 }
@@ -16,12 +16,12 @@ start();
 
 
 let appData = {
-    budget : money,
-    timeData : time,
+    budget: money,
+    timeData: time,
     expenses: {},
     optionalExpenses: {},
-    income : [],
-    savings : true
+    income: [],
+    savings: true
 };
 
 
@@ -29,16 +29,15 @@ function chooseExpenses() {
     for (let i = 0; i < 1; i++) {
         answer1 = prompt("Please, enter your binding expense this mounth");
         answer2 = prompt("How mutch is it?");
-        
-        if ( typeof(a) === 'string' && typeof(answer1) != null && typeof(answer2) != null
-        && answer1 != "" && answer2 != "" && answer1.lengt <= 50){
+
+        if (typeof (a) === 'string' && typeof (answer1) != null && typeof (answer2) != null &&
+            answer1 != "" && answer2 != "" && answer1.lengt <= 50) {
             console.log("done");
-            appData.expenses[answer1] = answer2; 
-        }
-        else {
+            appData.expenses[answer1] = answer2;
+        } else {
             --i;
         }
-    }    
+    }
 }
 
 
@@ -52,15 +51,15 @@ function detectDayBudget() {
 detectDayBudget();
 
 function detectLevel() {
-    if(appData.moneyPerDay > 100){
+    if (appData.moneyPerDay > 100) {
         console.log("Minimum level of wealth");
-    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
         console.log("Medium level of wealth");
-    } else if (appData.moneyPerDay > 2000){
+    } else if (appData.moneyPerDay > 2000) {
         console.log("High level of wealth");
     } else {
         console.log("Error !");
-    }    
+    }
 }
 
 detectLevel();
@@ -77,12 +76,12 @@ function chooseOptExpenses() {
 
 
 function checkSavings() {
-    if (appData.savings == true){
+    if (appData.savings == true) {
         let save = +prompt("What is amount of accumulated?"),
             percent = +prompt("What is per cent?");
 
 
-        appData.monthIncome = save/10000/12*percent;
+        appData.monthIncome = save / 10000 / 12 * percent;
         alert("Your mounth income: " + appData.monthIncome);
     }
 }
